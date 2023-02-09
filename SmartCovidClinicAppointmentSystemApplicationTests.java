381b17c3590848d23a4033c85f09489e2b97274e
ANITHA230520 <94451278+ANITHA230520@users.noreply.github.com>
2023-02-09 10:49:11 +0530

    Add files via upload

---
+++ SmartCovidClinicAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCovidClinicAppointmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
